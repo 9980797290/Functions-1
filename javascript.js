ce130d03c8dc8a982f56e31621830e5ed9a74ca5
Jhansi R <97968506+9980797290@users.noreply.github.com>
2022-04-05 11:56:01 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,112 @@
+// Functions
+// // Create one function with zero parameter having a console statement;
+// function x(){
+//     a=10;
+//     console.log(a)
+// }
+// x()
+
+
+// Create one function which takes two values as a parameter 
+// // and print the sum of them as "Sum of 3, 4 is 7"
+// function abc(a,b){
+// console.log(a+b)
+// }
+// abc(3,4)
+
+
+// Create one function which takes three parameter as first name and last name and
+//  age and return the statement as "A is 99 years old"
+// function details(){
+//    let firstname="harini";
+//    let lastname="reddy";
+//    let age=23;
+//    console.log(firstname, lastname    +'is'+  age, 'years old')
+// }
+// details()
+
+
+// // Create one arrow function
+// let adding=(e,f)=>{
+//  return e+f;
+// }
+// console.log(adding(2,3))
+
+
+
+// "Print output: 
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl();
+       // output is undefined
+
+
+// "Print output: "
+// var x = 21;
+// girl ();
+// console.log(x)
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };
+// the output to this is undefined and 21
+
+
+// "Print output
+// var x = 21;
+// a();
+// b();
+// console.log(a);
+// function a () {
+    
+//     var x = 20;
+// console.log(x);
+// };
+
+// function b () {
+    
+//     var x = 40;
+// console.log(x);
+// };
+
+// output to this is 20 and 40 and for console.log(a) is the defination of a function
+
+
+// "Print output"
+// var x = 21;
+// a();
+// b();
+// console.log(a);
+// a = function() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+// b = function() {
+    
+//     x = 40;
+//    console.log(x);
+// }
+// output is a is not defined
+
+// Write a function that accepts parameter n and returns factorial of n
+
+// function factorial(n){
+//    if(n == 0 || n == 1){
+//        return 1;
+//    }else{
+//        return n * factorial(n-1);
+//    }
+// }
+// result=factorial(4);
+// console.log(result);
+
+
+
+
+
+
+
